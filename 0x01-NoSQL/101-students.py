2f90fc87f4ff373c689e48c48507a4cb5d2b6c3f
Abeermohsien <aboorakhalil993@gmail.com>
2024-07-16 10:59:58 -0700

    nosql

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''module 14.
+'''
+
+
+def top_students(mongo_collection):
+    '''return students collection.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
